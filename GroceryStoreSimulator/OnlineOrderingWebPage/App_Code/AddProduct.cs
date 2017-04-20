b7849aab49d5640d2c7969db28ec15cf971e3a60
SirChibi <reilmajb@mail.uc.edu>
2017-04-19 20:42:47 -0400

    Added the AddProduct() class and method addProduct() in it.

---
+++ GroceryStoreSimulator/OnlineOrderingWebPage/App_Code/AddProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+/// <summary>
+/// Summary description for AddProduct
+/// </summary>
+public class AddProduct
+{
+    public AddProduct()
+    {
+
+    }
+    public void addProduct(TextBox qty, ListBox orderList)
+    {
+        ListItem productAndQty = new ListItem(orderList.SelectedItem.Text.PadRight(1, ':').PadRight(10, ' ') + qty.Text);
+        orderList.Items.Add(productAndQty);
+    }
+}
