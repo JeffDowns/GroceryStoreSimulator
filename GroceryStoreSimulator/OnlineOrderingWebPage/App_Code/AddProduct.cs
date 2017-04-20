6b2e46c84c79a558e9ceb9d89ce1a519c418d9d1
Jacob Reilman <reilmajb@mail.uc.edu>
2017-04-20 18:42:25 -0400

    Added comments

--- GroceryStoreSimulator/OnlineOrderingWebPage/App_Code/AddProduct.cs
+++ GroceryStoreSimulator/OnlineOrderingWebPage/App_Code/AddProduct.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+ * Group Project for IT3047
+ * Bill Nicholson
+ * nicholdw@ucmauil.uc.edu
+ * 
+ * /***********************************************************************************************************************************************************************************************
+ * Assignment 12
+ * Jake Reilman (reilmajb@mail.uc.edu) and Justin Meyer (meyer3js@mail.uc.edu)
+ * IT3047C Web Server App Dev
+ * Due Date: 4/19/2017
+ *
+ **********************************************************************************************************************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
